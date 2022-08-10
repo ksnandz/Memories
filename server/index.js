8e62a5e9aee6a09e50bbd5046cce9a3479e59611
Nandhakishore K.S <n.krishnamurthy@mail.utoronto.ca>
2022-08-09 21:52:56 -0400

    small refactor to make the forms UI

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://nandhuks:Achi98ksn$@cluster0.cpr7e.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
